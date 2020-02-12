7bb74ddb91e88ae88b4df59e39546466014dc7a9
yuki-s1027 <yuki.109921@gmail.com>
2020-02-12 16:02:24 +0900

    sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 6 }
-  validates :famiry_name        , presence: true
-  validates :first_name         , presence: true
-  validates :famiry_name_kana   , presence: true
-  validates :first_name_kana    , presence: true
+  validates :famiry_name        , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name         , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :famiry_name_kana   , presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana    , presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday           , presence: true
-  validates :postal_code        , presence: true
+  validates :postal_code        , presence: true, format: { with: /\A\d{7}\z/ }
   validates :adress_prefecture  , presence: true
   validates :adress_town        , presence: true
   validates :adress_number      , presence: true
