64b89a938d8c2ca340c629bb66d1cbe630315b50
hiraguchisayaka <59869369+hiraguchisayaka@users.noreply.github.com>
2020-02-18 18:10:00 +0900

    user-registration-remove

--- app/models/adress.rb
+++
@@ -1,9 +0,0 @@
-class Adress < ApplicationRecord
-  belongs_to :user
-
-
-  validates :postal_code        , presence: true, format: { with: /\A\d{7}\z/ }
-  validates :prefecture  , presence: true
-  validates :town        , presence: true
-  validates :number      , presence: true
-end
