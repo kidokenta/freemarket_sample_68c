d89df2501b3f71020d450185c49033924384409a
hiraguchisayaka <59869369+hiraguchisayaka@users.noreply.github.com>
2020-02-19 21:18:24 +0900

    remove

--- app/assets/javascripts/show_item_image.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(".image__sub__thumb").first().addClass("active"); 
-  $('.image__sub__thumb__photo').click(function () { 
-    image_url = $(this).attr("src"); 
-    $(".image__main__photo").attr("src", image_url).hide().fadeIn(); 
-    $(".image__sub__thumb.active").removeClass("active"); 
-    $(this).parent().addClass("active"); 
-  });
-});
