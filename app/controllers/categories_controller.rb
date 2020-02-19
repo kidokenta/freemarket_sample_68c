62bc4b0122fa4cf61d39cd510699accebb69eeb7
yuki-s1027 <yuki.109921@gmail.com>
2020-02-19 16:02:45 +0900

    test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -36,17 +36,6 @@ class CategoriesController < ApplicationController
     @list25s = Category.where(ancestry: "200/344")
   end
 
- # 以下全て、formatはjsonのみ
- # 親カテゴリーが選択された後に動くアクション
- def get_category_children
-    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
- end
 
- # 子カテゴリーが選択された後に動くアクション
- def get_category_grandchildren
-   #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-    @category_grandchildren = Category.find("#{params[:child_id]}").children
- end
 end
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,21 +11,21 @@ class ItemsController < ApplicationController
     @item = Item.new
     @category_parent_array = ["---"]
        #データベースから、親カテゴリーのみ抽出し、配列化
-          Category.where(ancestry: nil).each do |parent|
-             @category_parent_array << parent.name
-          end
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
   end
 
   def get_category_children
     #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
     @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
- end
+  end
 
  # 子カテゴリーが選択された後に動くアクション
- def get_category_grandchildren
+  def get_category_grandchildren
    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
     @category_grandchildren = Category.find("#{params[:child_id]}").children
- end
+  end
 
   def create
     @item = Item.create(item_params)
