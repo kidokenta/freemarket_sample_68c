12696e28b6f037261eefe783a198acc65d3de007
masa0150 <masa98989801@yahoo.co.jp>
2020-02-19 09:01:27 +0900

    おはようマージ！

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,11 @@ class CategoriesController < ApplicationController
 
   def index
     @parents = Category.all.order("id ASC").limit(13)
+    @tops = Category.where(ancestry: "1/2")
+    @jakets = Category.where(ancestry: "1/21")
+    @pants = Category.where(ancestry: "1/43")
+    @skirts = Category.where(ancestry: "1/56")
+    @onepieces = Category.where(ancestry: "1/62")
   end
   
 end
