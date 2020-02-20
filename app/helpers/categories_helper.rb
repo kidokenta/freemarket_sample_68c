52a27c094b629cfc42b83afc9c0a15ca052a27fe
masa0150 <masa98989801@yahoo.co.jp>
2020-02-20 17:10:19 +0900

    カテゴリー02

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,10 @@
 module CategoriesHelper
+
+  def category_aaa(parent)
+    @bbb = parent.id
+    @bbb += 1
+    @next_parent = Category.find_by(id: @bbb)
+    @ppp = @next_parent.ancestry
+    @vvv = Category.where(ancestry: @ppp)
+  end
 end
