d4fd0b72444cedff0da36b540c4a029f3dc71bb9
yuki-s1027 <yuki.109921@gmail.com>
2020-02-20 19:10:06 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    new_adress_path
+  end
+
   
   private
   def production?
