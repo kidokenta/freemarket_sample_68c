281b7c6548d25fcb2c2769a377624839ad7a0e04
kidokenta <george022497@gmail.com>
2020-02-20 12:07:33 +0900

    お願いします

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,18 @@ class Item < ApplicationRecord
   validates :condition, presence: true
   validates :shipping_fee, presence: true
   validates :shipping_days, presence: true
-  validates :shipping_company, presence: true
+  # validates :shipping_company, presence: true
   validates :shipping_region, presence: true
   validates :price, presence: true, length: { maximum: 6 ,minimum: 3}
   validates :seller_user_id, presence: true
+  validates :shipping_company, inclusion: { in: %w(small medium large),
+    message: "%{value}は有効な値ではありません" }, allow_nil: true
+    validates :brand, inclusion: { in: %w(small medium large),
+    message: "%{value}は有効な値ではありません" }, allow_nil: true
+    validates :category, inclusion: { in: %w(small medium large),
+    message: "%{value}は有効な値ではありません" }, allow_nil: true
+ 
+
 
 
   has_many :comments
