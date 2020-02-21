42996648dafc2f37f46237f9957d6b205b3a8d84
harurash <dgkteriyama@gmail.com>
2020-02-21 23:34:19 +0900

    test

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,12 +10,4 @@ class LikesController < ApplicationController
     @like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
     @like.destroy
   end
-
-  def show
-    @item = Item.all
-    @user = User.find_by(params[:id])
-    @items = @user.items
-    @like_items = @user.likes.map{|like| like.item}
-    # @like_items = @user.like_items
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find_by(params[:id])
+    @items = @user.items
+    @like_items = @user.like_items
   end
 end
