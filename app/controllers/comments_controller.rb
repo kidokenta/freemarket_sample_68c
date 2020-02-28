14b357912aaad4819b0ef9161b854d7212d80798
hiraguchisayaka <59869369+hiraguchisayaka@users.noreply.github.com>
2020-02-28 19:34:05 +0900

    remove comment-out

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,21 +18,6 @@ end
 end
 
 
-# def create
-#   @message = @group.messages.new(message_params)
-#   if @message.save
-#     respond_to do |format|
-#       format.json
-#     end
-#   else
-#     @messages = @group.messages.includes(:user)
-#     flash.now[:alert] = 'メッセージを入力してください。'
-#     render :index
-#   end
-# end
-
-
-
 private
 def comment_params
   params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
