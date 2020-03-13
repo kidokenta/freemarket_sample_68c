8a1ae267c37d813ea100d7ea41039d36abd774b9
yuki-s1027 <yuki.109921@gmail.com>
2020-03-13 14:41:05 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,23 +2,23 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :explain, presence: true, length: { maximum: 1000 }
   validates :status, presence: true
-  validates :size_id, presence: true
   validates :condition, presence: true
   validates :shipping_fee, presence: true
   validates :shipping_days, presence: true
   validates :shipping_region, presence: true
+  validates :price, presence: true, length: { maximum: 6 ,minimum: 3}
+  validates :seller_user_id, presence: true
   validates :category_id, presence: true
   validates :brand, presence: true, length: { maximum: 15 }
-  validates :price, presence: true, length: { maximum: 6 ,minimum: 3}
-  validates :category_id, presence: true
-  validates :seller_user_id, presence: true
+  validates :size_id, presence: true
+ 
 
   has_many :comments
   has_many :users
   has_many :likes
   has_many :images, dependent: :destroy
-  belongs_to :category ,optional:true
-  belongs_to :size, optional:true
+  belongs_to :category
+  belongs_to :size
 
   def self.search(search)
     return Item.all unless search
